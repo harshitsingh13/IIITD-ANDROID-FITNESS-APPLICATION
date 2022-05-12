660e46c59ed4ba1506b6ae62f478a5bf25d61c5a
Harshit <93035604+harshitsingh13@users.noreply.github.com>
2022-05-13 03:05:47 +0530

    new edit

--- app/src/main/java/com/mc2022/template/dietplans.java
+++ app/src/main/java/com/mc2022/template/dietplans.java
@@ -1,6 +1,5 @@
 package com.mc2022.template;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -14,25 +13,24 @@ import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link options#newInstance} factory method to
+ * Use the {@link dietplans#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class options extends Fragment {
+public class dietplans extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    Button b1,b2,b3,b4,b5,b6;
-    project1 p1=new project1();
-    mentalfitness mf=new mentalfitness();
-    dietplans plan=new dietplans();
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
-    public options() {
+    Button b1,b2,b3,b4,b5;
+    musclegain mgain=new musclegain();
+    weightgain wgain=new weightgain();
+    weightloss wloss=new weightloss();
+    public dietplans() {
         // Required empty public constructor
     }
 
@@ -42,11 +40,11 @@ public class options extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment options.
+     * @return A new instance of fragment dietplans.
      */
     // TODO: Rename and change types and number of parameters
-    public static options newInstance(String param1, String param2) {
-        options fragment = new options();
+    public static dietplans newInstance(String param1, String param2) {
+        dietplans fragment = new dietplans();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -63,19 +61,18 @@ public class options extends Fragment {
         }
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view=inflater.inflate(R.layout.fragment_options, container, false);
-        b1=(Button) view.findViewById(R.id.option1);//defining button for 'start service' button functionality
+        View view=inflater.inflate(R.layout.fragment_dietplans, container, false);
+        b1=(Button) view.findViewById(R.id.musgain);//defining button for 'start service' button functionality
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                fragmentTransaction.replace(R.id.frameLayout,p1);
+                fragmentTransaction.replace(R.id.frameLayout,mgain);
                 fragmentTransaction.addToBackStack(null);
                 fragmentTransaction.commit();
                 //Intent intent=new Intent(MainActivity.this,Service_1.class);
@@ -84,13 +81,13 @@ public class options extends Fragment {
             }
         });
 
-        b2=(Button) view.findViewById(R.id.option2);//defining button for 'start service' button functionality
+        b2=(Button) view.findViewById(R.id.wtgain);//defining button for 'start service' button functionality
         b2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                fragmentTransaction.replace(R.id.frameLayout,mf);
+                fragmentTransaction.replace(R.id.frameLayout,wgain);
                 fragmentTransaction.addToBackStack(null);
                 fragmentTransaction.commit();
                 //Intent intent=new Intent(MainActivity.this,Service_1.class);
@@ -99,28 +96,16 @@ public class options extends Fragment {
             }
         });
 
-        b6=(Button) view.findViewById(R.id.option6);//defining button for 'start service' button functionality
-        b6.setOnClickListener(new View.OnClickListener() {
+
+        b3=(Button) view.findViewById(R.id.wtloss);//defining button for 'start service' button functionality
+        b3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                fragmentTransaction.replace(R.id.frameLayout,plan);
+                fragmentTransaction.replace(R.id.frameLayout,wloss);
                 fragmentTransaction.addToBackStack(null);
                 fragmentTransaction.commit();
-                //Intent intent=new Intent(MainActivity.this,Service_1.class);
-                //intent.putStringArrayListExtra("newlist",list);
-                //startService(intent);
-            }
-        });
-
-
-        b5=(Button) view.findViewById(R.id.option5);//defining button for 'start service' button functionality
-        b5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(getActivity(),pedometer.class);
-                startActivity(intent);
                 //Intent intent=new Intent(MainActivity.this,Service_1.class);
                 //intent.putStringArrayListExtra("newlist",list);
                 //startService(intent);
