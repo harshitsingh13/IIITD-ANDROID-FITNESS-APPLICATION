14d751dba0779217752fa13e9cdf0436c888f98b
Rahul <rahul18077@iiitd.ac.in>
2022-05-12 20:28:14 +0530

    Project1

---
+++ app/src/main/java/com/mc2022/template/weight_gain.java
@@ -0,0 +1,14 @@
+package com.mc2022.template;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class weight_gain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weight_gain);
+    }
+}
